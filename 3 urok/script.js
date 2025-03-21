c49f72f734fa1488304631d6c7df99963fdf55e0
slafey <134937749+slafey@users.noreply.github.com>
2025-03-21 16:28:49 +0300

    3 urok

--- 3 urok/script.js
+++ 3 urok/script.js
@@ -1,20 +1,11 @@
+const form = document.querySelector('form');
 const day = document.getElementById('day');
 const month = document.getElementById('month');
 const year = document.getElementById('year');
 const result = document.getElementById('result');
 
-function getResult(type) {
-    if (type === 'day') {
-        console.log(`${day.value}`);
-    } else if (type === 'month') {
-        console.log(`${month.value}`);
-    } else if (type === 'year') {
-        console.log(`${year.value}`);
-    }
-
+function getResult() {
     result.innerHTML = `${day.value} ${month.value} ${year.value}`;
 }
 
-day.addEventListener('change', () => getResult('day'));
-month.addEventListener('change', () => getResult('month'));
-year.addEventListener('change', () => getResult('year'));
+form.addEventListener('change', getResult);
