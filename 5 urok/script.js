c2e4355eb6d0254db34b698e99590d9ee89415b9
slafey <134937749+slafey@users.noreply.github.com>
2025-08-23 19:59:34 +0300

    Update script.js

--- 5 urok/script.js
+++ 5 urok/script.js
@@ -1,5 +1,4 @@
 let border = document.querySelector('.road');
-border.style.border = '2px solid black';
 
 let white = document.querySelectorAll('.white');
 let black = document.querySelectorAll('.black');
@@ -7,9 +6,27 @@ let black = document.querySelectorAll('.black');
 let buttonUp = document.querySelector(".buttonUp");
 let buttonDown = document.querySelector(".buttonDown");
 
+border.style.border = '2px solid black';
+
 black.forEach(element => {
     element.style.backgroundColor = 'black'
 });
 
-buttonUp.addEventListener("click", () => console.log('Вверх'));
-buttonDown.addEventListener("click", () => console.log('Вниз'));
+function addArrow(direction) {
+    white.forEach(element => {
+        element.innerHTML = "";
+        let image = document.createElement("img");
+        image.src = 'arrow.svg';
+        image.style.display = "block";
+        image.style.margin = '0 auto';
+        image.style.width = "40px";
+        image.style.height = "40px";
+        if (direction === 'down') {
+            image.style.transform = 'rotate(180deg)';
+        }
+        element.appendChild(image);
+    });
+}
+
+buttonUp.addEventListener("click", () => addArrow('up'));
+buttonDown.addEventListener("click", () => addArrow('down'));
